018a8d4b576565e4851a5a9a9ffc2ad5ab55c99b
Ivelin Ivanov <inivanof@gmail.com>
2024-01-20 12:58:14 +0200

    PYChanges

--- Python/Basic/1. First Steps/15. Repainting.py
+++ Python/Basic/1. First Steps/15. Repainting.py
@@ -1,1 +1,20 @@
+nylon = int(input())
+paint = int(input())
+thinner = int(input())
+hours = int(input())
+
+price_nylon = 1.50
+price_paint = 14.50
+price_thinner = 5.00
+
+all_nylon = nylon * price_nylon
+all_paint = paint * price_paint
+all_thinner = thinner * price_thinner
+
+reserve = (all_paint * 10 / 100) + (2 * price_nylon) + 0.40
+all_price = all_nylon + all_paint + all_thinner + reserve
+price_human = all_price * 30 / 100
+all_work = all_price + (price_human * hours)
+
+print(all_work)
 

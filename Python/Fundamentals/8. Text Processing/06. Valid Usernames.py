cab23ff31b6148393632e2b039bfe9abeff8ee5f
Ivelin Ivanov <inivanof@gmail.com>
2024-03-10 16:07:28 +0200

    PYChanges

--- Python/Fundamentals/8. Text Processing/06. Valid Usernames.py
+++ Python/Fundamentals/8. Text Processing/06. Valid Usernames.py
@@ -1,1 +1,14 @@
+usernames = input().split(", ")
+
+for user in usernames:
+    if 3 < len(user) < 16:
+        if user.isalnum() or "-" in user or "_" in user:
+            print(user)
+
+
+
+
+
+
+
 
--- Python/Fundamentals/8. Text Processing/07. Character Multiplier.py
+++ Python/Fundamentals/8. Text Processing/07. Character Multiplier.py
@@ -1,1 +1,13 @@
+text = input().split(" ")
+num = 0
+num1 = 0
+for txt in text[0]:
+    for i in txt:
+        num += ord(i)
+
+for txt in text[1]:
+    for i in txt:
+        num1 += ord(i)
+
+print(num * num1)
 

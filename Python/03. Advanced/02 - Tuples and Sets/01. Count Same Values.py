8a71ae16996751f09b8456f814faed043dbe0541
Ivelin Ivanov <inivanof@gmail.com>
2024-06-10 23:06:37 +0300

    PYChanges

---
+++ Python/03. Advanced/02 - Tuples and Sets/01. Count Same Values.py
@@ -0,0 +1,13 @@
+numbers = tuple([float(num) for num in input().split()])
+
+
+occ = {}
+
+for num in numbers:
+    occ[num] = numbers.count(num)
+
+for key, value in occ.items():
+    print(f"{key:.1f} - {value} times")
+
+
+
